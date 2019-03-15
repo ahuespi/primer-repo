5edc155a342cbb91fa398586e3fd0d2263e5eecd
ahuespi <amir.huespi@gmail.com>
2019-03-14 22:01:48 -0300

    Integrando AJAX y Node.js a una API de Gdrive

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
-const http = require("http");
-const fs = require("fs");
+const http = require("http")
+const fs = require("fs")
+const path = require("path")
 
-http.createServer(function(request, response) {
-    const dir = "public/";
-    // const url = request.url;
+http.createServer(function(request, response){
+    
+    const dir = "public/" //<-- Definir el directorio de los archivos web
+    
+	//const url = request.url //<-- Leer la ruta/recurso solicitado en la URL
+    /*
+	if( request.url == "/" ){
+        const file = "index.html"
+	} else {
+        const file = request.url
+	}
+    */
+   //const file = (CONDICION) ? VERDADERO : FALSO //<-- Operador Ternario
+    const file = (request.url == "/") ? "index.html" : request.url
+    // let ext = String(path.extname(file)).toLowerCase(); <-- MODO NINJA
 
-    if (request.url == "/") {
-        const file = "index.html";
-    } else {
-        const file = request.url;
+    let ext = path.extname(file); 
+        ext = String(ext)
+        ext = ext.toLowerCase();
+    
+    let tipos = {
+        ".html"	: "text/html",
+        ".js"	: "text/javascript",
+        ".css"	: "text/css",
+        ".txt" 	: "text/plain",
+        ".json"	: "application/json",
+        ".png"	: "image/png",
+        ".jpg"	: "image/jpg",
+        ".gif"	: "image/gif",
+        ".ico"	: "image/x-icon",
+        ".wav"	: "audio/wav",
+        ".mp4"	: "video/mp4",
+        ".woff"	: "application/font-woff",
+        ".ttf"	: "application/font-ttf",
+        ".eot"	: "application/vnd.ms-fontobject",
+        ".otf"	: "application/font-otf",
+        ".svg"	: "application/image/svg+xml"
     }
 
-    const file = (request.url == "/") ? "index.html" : request.url;
+    let contentType = tipos[ext] || "application/octet-stream" // ESTO ES PARA QUE SE DESCARGUE AUTOMATICAMENTE EL ARCHIVO
 
-    
-    // fs.readFile(RUTA, ACCION);
-    fs.readFile(dir + file, function(error, content) {
-        if ( error ) { // <-- Si no pudo leer el archivo por X razon,
-            response.end("Archivo no encontrado :(")
-        } else {
-            response.writeHead(200, { "Content-Type" : "text/html" });
-            response.end(content);
-        }
-    });
-}).listen(80);
+	fs.readFile(dir + file, function(error, content){ //<-- "Intentar" leer/cargar el archivo/recurso solicitado
+
+		if( error ){ //<-- Si NO pudo leer el archivo por X razón...
+			response.end("ARCHIVO NO ENCONTRADO :(")
+		} else { //<-- Si efectivamente pudo leerlo...
+
+			response.writeHead(200, { "Content-Type" : contentType }) //<-- Especificar codigo de respuesta y tipo de contenido...
+
+			response.end(content) //<-- Enviar al cliente/navegador el archivo encontrado
+
+		}
+
+	})
+
+}).listen(80)
